ba9d19e140b3340750b5ca1365e1a49cea5c78b2
Mitch Shiotani <mitch.shiotani1@gmail.com>
2019-08-13 19:42:18 +0900

    created validations for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :supply
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :user_id, presence: true
+  validates :supply_id, presence: true
 end
