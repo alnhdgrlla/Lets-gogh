1f1590507c608099efc1043cfd9f9aaef063d579
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-13 21:42:20 +0900

    Created seeds for users and supplies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+
+puts "Getting some dummy users..."
+
+10.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(min_length: 6),
+    username: Faker::Internet.username
+  )
+  user.save!
+end
+
+puts "Goteem"
+
+Supply.destroy_all
+
+puts "Generating new supplies, please wait..."
+
+10.times do
+  supply = Supply.new(
+    title: Faker::Hipster.words(number: 3).join(" "),
+    price: Faker::Commerce.price(range: 0..500.0),
+    category: Supply::CATEGORY.sample,
+    description: Faker::Hipster.paragraphs(number: 2).join(" "),
+    user_id: (1..10).to_a.sample
+  )
+  supply.save!
+end
+
+puts "Supplies are ready!"
