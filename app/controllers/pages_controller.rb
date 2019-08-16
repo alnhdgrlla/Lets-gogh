5580c8f51615c52e1b53aab26458036f38c43cfc
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-08-16 19:03:45 +0900

    IT WORKS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
   def home
     if !params[:search].nil? && params[:search][:query].present?
       @supplies = policy_scope(Supply).search_by_category_title_desc(params[:search][:query])
+    elsif params[:cat_search].present?
+      @filter = params[:cat_search][:categories].flatten.reject(&:blank?).join(" ")
+      @supplies = policy_scope(Supply).search_by_category(@filter)
     elsif params[:tag].present?
       @supplies = policy_scope(Supply).tagged_with(params[:tag])
     else
